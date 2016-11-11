c96d7af4b8085d661071bf0980cc2af67426dc4f
Jenny Gebske <jennifer@gebske.de>
2016-11-11 20:27:31 +0100

    initial commit with unit tests

---
+++ src/index.js
@@ -0,0 +1,11 @@
+exports.findPositions = function findPositions(fullstr, searchstr) {
+  return [];
+};
+
+exports.transformSpaces = function transformSpaces(str) {
+  return {str: '', mapping: [{ transformed: 0, original: 0 }]};
+};
+
+exports.backTransformPositions = function backTransformPositions(positions, mapping) {
+  return [];
+};
---
+++ test/index.js
@@ -0,0 +1,44 @@
+const srch = require('../src/index');
+
+describe('findPositions()', () => {
+  it('should return zero position for entire string', () => {
+    srch.findPositions('test', 'test').should.eql([0]);
+  });
+
+  it('should return multiple adjacent positions', () => {
+    srch.findPositions('testtest', 'test').should.eql([0, 4]);
+  });
+
+  it('should return multiple positions', () => {
+    srch.findPositions('wow, test and test!', 'test').should.eql([5, 14]);
+  });
+});
+
+describe('transformSpaces', () => {
+  it('should remove spaces', () => {
+    srch.transformSpaces('this    is a test')
+      .should.eql({
+        str: 'this is a test',
+        mapping: [
+          { transformed: 5, original: 5 },
+          { transformed: 0, original: 3 },
+          { transformed: 9, original: 9 },
+        ],
+      });
+  });
+});
+
+describe('backTransformPositions()', () => {
+  it('should return the original positions', () => {
+    // original:    'hällo  world'
+    // transformed: 'haello world'
+    srch.backTransformPositions(
+      [0, 1, 2, 3, 4, 5, 6, 7, 8],
+      [
+        { transformed: 7, original: 6 },
+        { transformed: 0, original: 1 },
+        { transformed: 5, original: 5 },
+      ]
+    ).should.eql([0, 1, 1, 2, 3, 4, 5, 6, 7]);
+  });
+});
